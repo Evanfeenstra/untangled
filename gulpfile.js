5d0477cf8e4d589e010fd3b87aaa30964fdaaa66
Evan Feenstra <evan@evanfeenstra.com>
2018-12-15 01:22:42 -0800

    1

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+
+gulp.task('make-sw', () =>
+    gulp.src([
+        'node_modules/@babel/standalone/babel.min.js',
+        './lib/core.js',
+    ])
+    .pipe(concat('untied.sw.js'))
+    .pipe(gulp.dest('./dist/'))
+);
+
+gulp.task('make-client', () =>
+    gulp.src(['./lib/client.js'])
+    .pipe(concat('untied.client.js'))
+    .pipe(gulp.dest('./dist/'))
+);
+
+gulp.task('default', ['make-sw', 'make-client']);
